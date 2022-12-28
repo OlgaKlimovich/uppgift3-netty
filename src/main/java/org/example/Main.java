0f61179c46ccc9434e1518e8a008b6ebf9febf78
olga <oklimovich37@gmail.com>
2022-12-28 22:16:22 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.proxy.ReverseProxyServer;
+
+public class Main {
+    public static void main(String[] args) {
+        new ReverseProxyServer(8000).start();
+    }
+}
